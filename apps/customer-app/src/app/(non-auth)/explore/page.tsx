4880ce27c1acd181abb531338f25d11454cf88e6
rukkiechovwe <rukkiechowe@gmail.com>
2024-12-04 20:53:49 +0100

    fix build error

--- apps/customer-app/src/app/(non-auth)/explore/page.tsx
+++ apps/customer-app/src/app/(non-auth)/explore/page.tsx
@@ -1,7 +1,26 @@
-"use client";
-
 import ExplorePageLayout from "@/components/Explore/ExplorePageLayout";
 
-export default function SearchVehiclesPage() {
-  return <ExplorePageLayout type="search" />;
+export default async function SearchVehiclesPage({
+  searchParams,
+}: {
+  searchParams: {
+    search?: string;
+    fromDate?: string;
+    fromTime?: string;
+    untilDate?: string;
+    untilTime?: string;
+  };
+}) {
+  const { search, fromDate, fromTime, untilDate, untilTime } = searchParams;
+
+  return (
+    <ExplorePageLayout
+      type="search"
+      search={search}
+      fromDate={fromDate}
+      fromTime={fromTime}
+      untilDate={untilDate}
+      untilTime={untilTime}
+    />
+  );
 }
